3232bca996c975f6e0ab3082f71f3e9a95bff90d
Michael Ellis <mellis@anexinet.com>
2020-05-22 09:56:05 -0400

    Error repro

--- src/root-config.js
+++ src/root-config.js
@@ -1,24 +1,10 @@
 import { registerApplication, start } from "single-spa";
 import * as isActive from "./activity-functions";
 
-// Simple usage
-registerApplication(
-  "@mf-demo/navbar",
-  () => System.import("@mf-demo/navbar"),
-  isActive.navbar
-);
-
-//Config with more expressive API
 registerApplication({
-  name: "@mf-demo/employees",
-  app: () => System.import("@mf-demo/employees"),
-  activeWhen: location => location.pathname === "/employees"
-});
-
-registerApplication({
-  name: "@mf-demo/employee-details",
-  app: () => System.import("@mf-demo/employee-details"),
-  activeWhen: isActive.employeeDetails
+  name: "@mf-demo/navbar",
+  app: () => System.import("@mf-demo/navbar"),
+  activeWhen: isActive.navbar,
 });
 
 start();
