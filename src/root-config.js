763f88dd05948847e32109878ef0bae49cd9bd10
Michael Ellis <michael.ellis@vertexinc.com>
2020-11-17 11:13:28 -0500

    Removes POC stuff

--- src/root-config.js
+++ src/root-config.js
@@ -1,15 +1,5 @@
 import { registerApplication, start } from "single-spa";
 
-const microfrontends = ["@mf-demo/navbar", "@mf-demo/employees", "@mf-demo/employee-details"]
-
-const contentRootElement = document.getElementById('mf-content');
-microfrontends.forEach(name => {
-  const microFrontendElement = document.createElement('div');
-  // microFrontendElement.attachShadow({ mode: 'open' });
-  microFrontendElement.setAttribute('id', `single-spa-application:${name}`);
-  contentRootElement.appendChild(microFrontendElement);
-})
-
 registerApplication({
   name: "@mf-demo/navbar",
   app: () => System.import("@mf-demo/navbar"),
